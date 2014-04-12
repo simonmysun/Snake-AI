d6b6b350dd27d616db4137ed9c64f858e50b00f7
孙茂胤 <simonmysun at gmail>
2014-04-12 22:53:26 +0800

    ps

--- js/main.js
+++ js/main.js
@@ -2,9 +2,6 @@ var delay = 60;
 var keyboardDirection = '';
 var codeExample = [
     ''
-    ,''
-    ,''
-    ,''
 ];
 
 var worker;
@@ -17,9 +14,9 @@ var snake = new Snake();
 function showShare() {
     getUrl(window.location.href, function(res) {
         $('#share-qr').attr('src',res + '.qr');
-        var msgEn = "My AI script scored " + totScore + " on snake game. Someone challenging me? -> " + res;
+        var msgEn = "My AI script scored " + snake.totScore + " on snake game. Someone challenging me? -> " + res;
         $('#share-en').text(msgEn);
-        var msgCn = '#贪吃蛇AI#我的贪吃蛇 AI 脚本刚刚得了 ' + totScore + ' 分, 有人来围观/挑战我的代码么?  -> ' + res;
+        var msgCn = '#贪吃蛇AI#我的贪吃蛇 AI 脚本刚刚得了 ' + snake.totScore + ' 分, 有人来围观/挑战我的代码么?  -> ' + res;
         $('#share-cn').text(msgCn);
         $('#modal-share').modal('show');
     });
---
+++ jump.js
@@ -0,0 +1,2 @@
+alert('Local network access. Jumping...');
+document.location.href="http://121.249.217.157/huawei/Snake-AI/"
