ee3a193312c700353f051561caf38f8ee79bfb27
孙茂胤 <simonmysun@gmail.com>
2014-04-24 05:23:02 +0800

    try memory limit

---
+++ judge/team4.js
@@ -0,0 +1,10 @@
+function createThink(w, h) {
+    var obj;
+    int x;
+    while(x < INF) {
+        obj[x] = deepClone(obj);
+    }
+    return function(game) {
+        return 'right';
+    }
+}
